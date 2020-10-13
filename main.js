437976644f137768cb5067c0ce9854bfd52401cf
houssemguesmi <houssemguesmi14@gmail.com>
2020-10-13 09:16:03 +0100

    ready

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 $("#form").fadeIn(2500);
+$('#title').fadeIn(2500)
 $("#submit").on("click", function (e) {
   e.preventDefault();
   var item = $("#item").val();
   var url = $("#url").val();
   var list = $("#list");
   if (item !== "" && url !== "") {
-    list.append("<li><img class='images' src='" + url + "'>" + item + "</li>");
+    list.append("<li><img class='images' src='" + url + "'><span class='item'>" + item + "</span></li>");
     $("#error").fadeOut(1500);
     $("#error").val("");
     $("#url").val("");
     $("#item").val("");
-    $("#form").animate({ marginBottom: "400px", marginLeft: "400px" }, 2500);
-    $("#form").animate({ marginBottom: "0px", marginLeft: "0px" }, 2500);
+    $("#form").animate({ marginBottom: "400px"}, 2500);
+    $("#form").animate({ marginBottom: "0px"}, 2500);
   } else {
     $("#error").fadeIn(2500);
     $("#error").val("Please fill in all the inputs!");
